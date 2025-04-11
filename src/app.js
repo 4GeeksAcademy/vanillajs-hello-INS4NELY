3911c74d75f79ec4affc6707cded1344a92f586f
ff.mendieta <82422221+INS4NELY@users.noreply.github.com>
2025-04-11 16:53:06 +0000

    finish project card generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,46 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let palo = ['♦', '♥', '♠ ', '♣']
+  let paloDefinido = palo[Math.floor(Math.random()* palo.length)]
+  let numeros = [2,3,4,5,6,7,8,9,10,'J', 'Q', 'K']
+  let segundos = 10;
+  document.getElementById('palo-izquierdo').innerHTML = paloDefinido;
+  document.getElementById('numero').innerHTML = numeros[Math.floor(Math.random()* numeros.length)];
+  document.getElementById('palo-derecho').innerHTML = paloDefinido;
+
+  generate.addEventListener("click", function() {
+    let paloDefinido = palo[Math.floor(Math.random()* palo.length)]
+    document.getElementById('palo-izquierdo').innerHTML = paloDefinido;
+    document.getElementById('numero').innerHTML = numeros[Math.floor(Math.random()* numeros.length)];
+    document.getElementById('palo-derecho').innerHTML = paloDefinido;
+    segundos = 10;
+
+  });
+
+  function cuentaRegresiva() {
+    document.getElementById("contador").innerText = segundos;
+    if (segundos > 0) {
+      segundos--;
+      setTimeout(cuentaRegresiva, 1000);
+    } else {
+      let paloDefinido = palo[Math.floor(Math.random()* palo.length)]
+      document.getElementById('palo-izquierdo').innerHTML = paloDefinido;
+      document.getElementById('numero').innerHTML = numeros[Math.floor(Math.random()* numeros.length)];
+      document.getElementById('palo-derecho').innerHTML = paloDefinido;
+      segundos = 10;
+      setTimeout(cuentaRegresiva, 1000);
+    }
+  }
+  cuentaRegresiva();
+
+  document.getElementById("reSizes").addEventListener("click", function () {
+    let ancho = document.getElementById("ancho").value;
+    let largo = document.getElementById("largo").value;
+    let carta = document.getElementById("carta");
+    if (!ancho.endsWith("rem")) ancho += "rem";
+    if (!largo.endsWith("rem")) largo += "rem";
+    carta.style.width = ancho;
+    carta.style.height = largo;
+  });
 };
